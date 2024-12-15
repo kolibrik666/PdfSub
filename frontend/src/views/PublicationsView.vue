1da8c5c5de10b22024a52247fa4bd6142f5bb37e
kolibrik666 <58887215+kolibrik666@users.noreply.github.com>
2024-12-15 11:22:01 +0100

    Better code

--- frontend/src/views/PublicationsView.vue
+++ frontend/src/views/PublicationsView.vue
@@ -112,6 +112,7 @@
 </template>
 <script>
 import api from '../services/api';
+import { decodeTokenUpdateData } from '../services/tokenUtils';
 
 export default {
   data() {
@@ -171,22 +172,9 @@ export default {
     setUserRole() {
       const token = localStorage.getItem('userToken');
       if (token) {
-        this.decodeTokenUpdateData(token);
+        decodeTokenUpdateData(token, this);
+        this.isLoggedIn = true;
       }
-    },
-    decodeTokenUpdateData(token) {
-      api.decode_token({ token })
-        .then(response => {
-          const decodedToken = response.data.user;
-          this.isAdmin = decodedToken.isAdmin;
-          this.isParticipant = decodedToken.isParticipant;
-          this.isReviewer = decodedToken.isReviewer;
-          this.isLoggedIn = true;
-          this.user_id = decodedToken.id;
-        })
-        .catch(error => {
-          console.error("Token decoding failed", error.response ? error.response.data : error.message);
-        });
     },
     submitPublication(publication) {
       if (this.isBeforeDeadline()) {
